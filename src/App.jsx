4092b2fe3540a40739e37f847d50ca83b2a75401
Igor Tintor <igortintor99@gmail.com>
2025-03-07 11:57:47 +0100

    added basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
+      <main>
+        <section><Home /></section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+      <Footer />
     </>
   )
 }
