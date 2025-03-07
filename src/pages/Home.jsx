f76117ecf84741fbe3b6ce1229aa25c17d9be75c
McTintor <igortintor99@gmail.com>
2025-03-07 15:12:56 +0100

    added hero img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import redhouse from '../assets/redhouse.png';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
+            <img src={redhouse} alt="Red House" />
             <h1>Welcome to the Home Page</h1>
             <p>This is the home page of our application.</p>
         </div>
